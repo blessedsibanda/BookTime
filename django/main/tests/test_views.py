b4d541b7f528fd2b26f65460a060a5cf971b3168
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-09-03 11:01:26 +0200

    contact us page

--- django/main/tests/test_views.py
+++ django/main/tests/test_views.py
@@ -1,16 +1,27 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from main import forms
+
 
 class TestPage(TestCase):
     def test_home_page_works(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(200, response.status_code)
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'main/home.html')
         self.assertContains(response, 'BookTime')
 
     def test_about_us_page_works(self):
         response = self.client.get(reverse('about_us'))
         self.assertEqual(200, response.status_code)
-        self.assertTemplateUsed(response, 'about_us.html')
+        self.assertTemplateUsed(response, 'main/about_us.html')
         self.assertContains(response, 'BookTime')
+
+    def test_contact_us_page_works(self):
+        response = self.client.get(reverse('contact_us'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/contact_form.html')
+        self.assertContains(response, 'BookTime')
+        self.assertIsInstance(
+            response.context['form'], forms.ContactForm
+        )
