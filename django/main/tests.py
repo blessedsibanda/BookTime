04aba63b49d8a69f431eb4897739d3f2cb5be067
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-09-03 09:35:41 +0200

    test home page

--- django/main/tests.py
+++ django/main/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestPage(TestCase):
+    def test_home_page_works(self):
+        response = self.client.get('/')
+        self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'BookTime')
