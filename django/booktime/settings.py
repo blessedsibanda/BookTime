d77c78b5bd2dcb1d06fb96d07d559d0e6e75acc8
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-09-03 09:04:14 +0200

    configure postgres db

--- django/booktime/settings.py
+++ django/booktime/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'booktime.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'booktime',
+        'USER': 'booktime',
+        'PASSWORD': 'development',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -150,3 +154,7 @@ LOGGING = {
         },
     },
 }
+
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
