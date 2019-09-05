f046dac334b007b01bc703dd46162d6bf8640ea2
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-09-05 14:02:01 +0200

    merge baskets at login

--- django/main/urls.py
+++ django/main/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('address/<int:pk>/delete', views.AddressDeleteView.as_view(),
         name='address_delete'),
     path('add_to_basket/', views.add_to_basket, name='add_to_basket'),
+    path('basket/', views.manage_basket, name='basket'),
     path('',
         TemplateView.as_view(template_name='home.html'),
         name='home'),
