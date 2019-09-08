274dbc63da7c5159a59367bc4ad0711037c86e36
Blessed Sibanda <n01519451g@students.nust.ac.zw>
2019-09-08 13:12:57 +0200

    add custom properties to user model

--- django/main/models.py
+++ django/main/models.py
@@ -99,6 +99,14 @@ class User(AbstractUser):
 
     objects = UserManager()
 
+    @property
+    def is_employee(self):
+        return self.is_active and (
+            self.is_superuser
+            or self.is_staff
+            and self.groups.filter(name='Employees').exists()
+        )
+
 
 class Address(models.Model):
     SUPPORTED_COUNTRIES = (
